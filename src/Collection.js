66c6e84384c5e9dbfcd47cde8b89e6ff3e6e66ae
Norman Rusch <norman.rusch@moccu.com>
2018-02-22 17:12:15 +0100

    Fix get handler when property is a getter instead of a function in collection

--- src/Collection.js
+++ src/Collection.js
@@ -17,42 +17,46 @@ export class Collection extends EventEmitter {
 				get: (target, property) => {
 					const method = target[property];
 
-					return (...args) => {
-						let
-							isChanged = false,
-							result
-						;
+					if (typeof method === 'function') {
+						return (...args) => {
+							let
+								isChanged = false,
+								result
+							;
 
-						switch (property) {
-							case 'pop':
-							case 'shift':
-							case 'reverse':
-							case 'sort':
-								isChanged = true;
-								break;
+							switch (property) {
+								case 'pop':
+								case 'shift':
+								case 'reverse':
+								case 'sort':
+									isChanged = true;
+									break;
 
-							case 'fill':
-							case 'push':
-							case 'unshift':
-								isChanged = true;
-								args = args.map(enshureIsModel);
-								break;
+								case 'fill':
+								case 'push':
+								case 'unshift':
+									isChanged = true;
+									args = args.map(enshureIsModel);
+									break;
 
-							case 'splice':
-								isChanged = true;
-								args = args.map((arg, index) =>
-									(index > 1) ? enshureIsModel(arg) : arg);
-								break;
-						}
+								case 'splice':
+									isChanged = true;
+									args = args.map((arg, index) =>
+										(index > 1) ? enshureIsModel(arg) : arg);
+									break;
+							}
 
-						result = method.apply(target, args);
+							result = method.apply(target, args);
 
-						if (isChanged) {
-							this.trigger('change', {method: property});
-						}
+							if (isChanged) {
+								this.trigger('change', {method: property});
+							}
 
-						return result;
-					};
+							return result;
+						};
+					}
+
+					return method;
 				}
 			},
 			proxy = new Proxy(models.map(enshureIsModel), handler)

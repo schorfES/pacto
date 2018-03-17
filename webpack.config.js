9fd6c68ae0b517e9b4cc3a33a62068c8ec61c22d
Norman Rusch <me@nrmnrsh.com>
2018-03-17 20:41:48 +0100

    Setup build process

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+
+module.exports ={
+	mode: 'production',
+	entry: './src/index.js',
+	output: {
+		filename: 'pacto.min.js',
+		path: path.resolve(__dirname, 'dist'),
+		library: 'pacto',
+		libraryTarget: 'umd'
+	}
+};

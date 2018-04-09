80c954a53ff101ffa00406e8aec00092385b319b
Norman Rusch <norman.rusch@moccu.com>
2018-04-09 19:34:43 +0200

    Expose new classes
    * View
    * Initialize
    * InitializeLazy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import {Collection} from './Collection';
 import {Context} from './Context';
 import {EventEmitter} from './EventEmitter';
+import {Initialize} from './actions/Initialize';
+import {InitializeLazy} from './actions/InitializeLazy';
 import {Model} from './Model';
+import {View} from './View';
 
-export {Collection, Context, EventEmitter, Model};
+export {
+	Collection,
+	Context,
+	EventEmitter,
+	Initialize,
+	InitializeLazy,
+	Model,
+	View
+};
